4c7821ed12ae2241975741ffe3f8205033eafbb1
Manuel Guesdon <mguesdon.gna@orange-concept.com>
2003-02-21 18:36:31 +0000

    Removed test code
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gsldapwebexplorer/trunk@16039 72102866-910b-0410-8b05-ffd578937521

--- GSLDAPWebExplorer_main.m
+++ GSLDAPWebExplorer_main.m
@@ -40,60 +40,6 @@ int main(int argc, char** argv, char** env)
 #endif
   //[NSObject enableDoubleReleaseCheck:YES];
 
-  {
-    NSString* str=@"( 1.3.6.1.4.1.4980.1.3.3 NAME 'oxyInetDomain' DESC 'Oxymium Inet Domain' SUP oxyTop AUXILIARY MUST dc MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )";
-      NSScanner *aScanner = [NSScanner scannerWithString:str];
-
-      if (1)
-        {
-          BOOL upTo1=[aScanner scanUpToString:@"MUST "
-                               intoString:NULL];
-          NSLog(@"upTo1=%d",upTo1);
-          if (upTo1)
-            {
-              BOOL scan1=[aScanner scanString:@"MUST "
-                                   intoString:NULL];
-              NSLog(@"scan1=%d",scan1);
-              if (scan1)
-                {
-                  NSString* must=nil;
-                  [aScanner scanUpToString:@" "
-                            intoString:&must];
-                }
-            };
-        };
-      NSLog(@"scanLocation=%u",[aScanner scanLocation]);
-      NSLog(@"scan=[%@]",[[aScanner string] substringFromIndex:[aScanner scanLocation]]);
-      BOOL upTo2=[aScanner scanUpToString:@"MAY "
-                          intoString:NULL];
-      NSLog(@"upTo2=%d",upTo2);
-      NSLog(@"scanLocation=%u",[aScanner scanLocation]);
-      NSLog(@"scan=[%@]",[[aScanner string] substringFromIndex:[aScanner scanLocation]]);
-      upTo2=[aScanner scanString:@"MAY"
-                      intoString:NULL];
-      NSLog(@"upTo2=%d",upTo2);
-      if (upTo2)
-        {
-          BOOL scan2=[aScanner scanString:@"MAY"
-                               intoString:NULL];
-          NSLog(@"scan2=%d",scan2);
-        };
-  };
-/*
-2003-02-19 10:31:28.489 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 52. In [NSScanner -scanThruAndDiscardString:] scan=[ MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )]
-2003-02-19 10:31:28.489 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 53. In [NSScanner -scanThruAndDiscardString:] startLoc=101
-2003-02-19 10:31:28.489 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 56. In [NSScanner -scanThruAndDiscardString:] ok1=0
-2003-02-19 10:31:28.489 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 50. In [NSScanner -scanThruAndDiscardString:] string=MAY 
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 51. In [NSScanner -scanThruAndDiscardString:] scan str=[( 1.3.6.1.4.1.4980.1.3.3 NAME 'oxyInetDomain' DESC 'Oxymium Inet Domain' SUP oxyTop AUXILIARY MUST dc MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )]
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 52. In [NSScanner -scanThruAndDiscardString:] scan=[ MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )]
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 53. In [NSScanner -scanThruAndDiscardString:] startLoc=101
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 56. In [NSScanner -scanThruAndDiscardString:] ok1=0
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 50. In [NSScanner -scanThruAndDiscardString:] string= MAY (
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 51. In [NSScanner -scanThruAndDiscardString:] scan str=[( 1.3.6.1.4.1.4980.1.3.3 NAME 'oxyInetDomain' DESC 'Oxymium Inet Domain' SUP oxyTop AUXILIARY MUST dc MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )]
-2003-02-19 10:31:28.490 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 52. In [NSScanner -scanThruAndDiscardString:] scan=[ MAY ( admContact $ admincontact $ billingContact $ cn $ domainCreationDate $ labeledURL $ mailRelayedBy $ managerEMail $ nic $ nichandle $ registrant $ soa $ techContact $ domaindescription $ oxymiumReferenceDisplay $ associatedServices $ associatedrootentity $ associatedmanagemententity $ associatedrootcompany $ associatedmanagemententityrights $ associatedrootentityrights $ associatedrootcompanyrights $ parkmessage $ logoURL ) )]
-2003-02-19 10:31:28.491 GSLDAPWebExplorer[11494] File GSLDAPUtils.m: 53. In [NSScanner -scanThruAndDiscardString:] startLoc=101
-*/
-
   GSWApplicationMain(@"GSLDAPWebExplorer",
                      argc,
                      argv);
